0b4da5f1503985caee2542729aa3e018571496bc
Camille_Ricardon <camille.ricardon@epitech.eu>
2024-07-12 15:50:45 +0200

    [+] add more routes for more requests

--- points-project/backend/index.js
+++ points-project/backend/index.js
@@ -9,6 +9,7 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
+//Request for the teams
 app.get('/api/starter_teams', async (req, res) => {
     const teams = await Teams.getStarter();
     res.json(teams);
@@ -22,6 +23,24 @@ app.get('/api/factions_teams', async (req, res) => {
 app.get('/api/all_teams', async (req, res) => {
     const teams = await Teams.getAll();
     res.json(teams);
+});
+
+app.post('/api/add_team', async (req, res) => {
+    const { name, color } = req.body;
+    const team = await Teams.addTeam(name, color);
+    res.json(team);
+});
+
+app.put('/api/change_team', async (req, res) => {
+    const { team_id, name, color } = req.body;
+    const team = await Teams.changeTeam(team_id, name, color);
+    res.json(team);
+});
+
+app.delete('/api/delete_team', async (req, res) => {
+    const { team_id } = req.body;
+    const team = await Teams.deleteTeam(team_id);
+    res.json(team);
 });
 
 app.listen(port, () => {
--- points-project/backend/models/Teams.js
+++ points-project/backend/models/Teams.js
@@ -35,4 +35,22 @@ Teams.getAll = async function() {
     return results;
 }
 
+Teams.deleteTeam = async function(team_id) {
+    const sql = `DELETE FROM TEAMS WHERE team_id = ${team_id};`;
+    const [results, metadata] = await sequelize.query(sql);
+    return results;
+}
+
+Teams.addTeam = async function(name, color) {
+    const sql = `INSERT INTO TEAMS (name, color) VALUES ('${name}', '${color}');`;
+    const [results, metadata] = await sequelize.query(sql);
+    return results;
+}
+
+Teams.changeTeam = async function(team_id, name, color) {
+    const sql = `UPDATE TEAMS SET name = '${name}', color = '${color}' WHERE team_id = ${team_id};`;
+    const [results, metadata] = await sequelize.query(sql);
+    return results;
+}
+
 module.exports = Teams;

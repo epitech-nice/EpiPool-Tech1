4f6a352ea807db8988e0e73600c1dbf51d6faa5a
Camille_Ricardon <camille.ricardon@epitech.eu>
2024-07-30 11:05:09 +0200

    [+] add request to add logs in DB

--- points-project/backend/controllers/logsController.js
+++ points-project/backend/controllers/logsController.js
@@ -30,3 +30,17 @@ exports.getLogsByStudent = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
+
+exports.addLog = async (req, res) => {
+    try {
+        const { team_id, student_id, points, event } = req.body;
+        if (!team_id && !student_id)
+            return res.status(400).json({ error: 'team_id or student_id are required' });
+        if (!event)
+            event = "Pedago";
+        const log = await Logs.addLog(team_id, student_id, points, event);
+        res.json(log);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};

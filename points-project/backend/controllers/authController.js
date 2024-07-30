3f93a582932d1ab34edfc62c35f5890714ac1595
Camille_Ricardon <camille.ricardon@epitech.eu>
2024-07-30 17:59:35 +0200

    [+] try to add authen thanks to JWT

--- points-project/backend/controllers/authController.js
+++ points-project/backend/controllers/authController.js
@@ -1,19 +1,19 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/Users');
 
-const secretKey = 'your_secret_key';
+const secretKey = process.env.JWT_SECRET;
 
 exports.login = async (req, res) => {
     const { email, password } = req.body;
 
     try {
         const user = await User.findOne({ where: { email } });
-        if (!user) return res.status(400).json({ error: 'Invalid email or password' });
-
+        if (!user)
+            return res.status(400).json({ error: 'Invalid email or password' });
         const validPassword = await bcrypt.compare(password, user.password);
-        if (!validPassword) return res.status(400).json({ error: 'Invalid email or password' });
-
+        if (!validPassword)
+            return res.status(400).json({ error: 'Invalid email or password' });
         const token = jwt.sign({ id: user.id, email: user.email }, secretKey, { expiresIn: '1h' });
         res.json({ token });
     } catch (error) {
